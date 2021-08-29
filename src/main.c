916ca79ec7a7866c36e66a2e6fb92c9bfad08756
rofl0r <rofl0r@users.noreply.github.com>
2021-08-29 17:06:28 +0000

    clang-format fixup for full-screen toggle

--- src/main.c
+++ src/main.c
@@ -141,7 +141,8 @@ int main(int argc, char *argv[])
                     vm->memory.mem[0xff0f] |= 0x10;
                     break;
                 case SDL_SCANCODE_RETURN: /* start button */
-                    if (evt.key.keysym.mod & KMOD_LALT || evt.key.keysym.mod & KMOD_RALT)
+                    if (evt.key.keysym.mod & KMOD_LALT ||
+                        evt.key.keysym.mod & KMOD_RALT)
                         toggle_fullscreen(&vm->lcd);
                     else {
                         vm->state.keys.state |= GB_KEY_START;
